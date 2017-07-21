bf7d81b73c3cc85aea608cd9b289389739283641
Eugen Stotik <eugen_stotik@epam.com>
2017-07-21 16:13:57 +0300

    Refactor

--- spec.js
+++ spec.js
@@ -12,6 +12,7 @@ describe('comparison two iPhones scenario', function() {
     console.log('\n...');
   });
 
+
   describe('adding Apple iPhone 6s 16GB Silver', function() {
 
     it('should have a proper title', function() {
@@ -19,110 +20,46 @@ describe('comparison two iPhones scenario', function() {
     });
 
     it('should choose catalog', function() {
-      var element = browser.element(by.linkText("Каталог"));
-    	     element.click();
-
-      var element = browser.element(by.linkText("Мобильные телефоны"));
-      		browser.wait(EC.elementToBeClickable(element), 5000)
-            .then(()=> {
-              element.click();
-            })
-          expect(browser.getTitle()).toEqual('Мобильный телефон купить в Минске');
+      pathToMobiles();
     });
 
     it('should setting up filter', function() {
-      var cb = browser.element.all(by.cssContainingText('span', "Apple")).first();
-      browser.driver.executeScript("arguments[0].scrollIntoView();", cb.getWebElement())
-        .then(()=> {
-           cb.click();
-        })
-        .then(()=> {
-          var condition = EC.elementToBeClickable(browser.$('.schema-tags__text'));
-             browser.wait(condition, 5000);
-        })
+      settingUpFilter("Apple");
     });
 
-      it('should find and choose phone Apple iPhone 6s 16GB Silver', function() {
-        // crutch
-        browser.sleep(2000);
-
-        var element = browser.element(by.cssContainingText('span', 'Apple iPhone 6s 16GB Silver'));
-        browser.wait(EC.presenceOf(element), 8000).then(()=> {
-            browser.executeScript("arguments[0].scrollIntoView();", element);
-            return element.click();
-        })
-        expect(browser.getTitle()).toEqual('Смартфон Apple iPhone 6s 16GB Silver купить в Минске');
-      });
+    it('should find and choose phone Apple iPhone 6s 16GB Silver', function() {
+      findPhone('Apple iPhone 6s 16GB Silver');
+    });
 
     it('should be added to comparison', function () {
-      var element = browser.$('#product-compare-control');
-      browser.wait(EC.presenceOf(element), 5000).then(()=> {
-        element.click();
-      });
-
-      var panel = browser.$('.compare-button__sub.compare-button__sub_main');
-        return panel.getText()
-          .then((text)=> {
-            console.log("panel text: ", text);
-            expect(text).toEqual('1 товар в сравнении');
-          });
+      var result = addToComparison();
+      expect(result).toEqual('1 товар в сравнении');
     });
 
   });
 
-  describe('adding Apple iPhone 6s 16GB Silver', function() {
+
+  describe('adding Apple iPhone SE 16GB Space Gray', function() {
 
     it('should choose catalog', function() {
-      var element = browser.element(by.linkText("Каталог"));
-           element.click();
-
-      var element = browser.element(by.linkText("Мобильные телефоны"));
-          browser.wait(EC.elementToBeClickable(element), 5000)
-            .then(()=> {
-              element.click();
-            })
-          expect(browser.getTitle()).toEqual('Мобильный телефон купить в Минске');
+      pathToMobiles();
     });
 
-    it('should setting up filter again', function() {
-      var cb = browser.element.all(by.cssContainingText('span', "Apple")).first();
-      browser.driver.executeScript("arguments[0].scrollIntoView();", cb.getWebElement())
-        .then(()=> {
-           cb.click();
-        })
-        .then(()=> {
-          var condition = EC.elementToBeClickable(browser.$('.schema-tags__text'));
-             browser.wait(condition, 5000);
-        })
+    it('should setting up filter', function() {
+      settingUpFilter("Apple")
     });
 
     it('should find and choose phone Apple iPhone SE 16GB Space Gray', function() {
-      // idk
-      browser.sleep(2000);
-
-      var element = browser.element(by.cssContainingText('span', 'Apple iPhone SE 16GB Space Gray'));
-      browser.wait(EC.presenceOf(element), 8000).then(()=> {
-          browser.executeScript("arguments[0].scrollIntoView();", element);
-          return element.click();
-      })
-      expect(browser.getTitle()).toEqual('Смартфон Apple iPhone SE 16GB Space Gray купить в Минске');
+      findPhone('Apple iPhone SE 16GB Space Gray');
     });
 
     it('should be added to comparison', function () {
-      var element = browser.$('#product-compare-control');
-      browser.wait(EC.presenceOf(element), 5000).then(()=> {
-        element.click();
-      });
-
-      var panel = browser.$('.compare-button__sub.compare-button__sub_main');
-        return panel.getText()
-          .then((text)=> {
-            console.log("panel text: ", text);
-            expect(text).toEqual('2 товара в сравнении');
-          });
+      var result = addToComparison();
+      expect(result).toEqual('2 товара в сравнении');;
     });
 
   });
+
 
   describe('checking the comparison', function() {
 
@@ -163,5 +100,58 @@ describe('comparison two iPhones scenario', function() {
       expect(browser.getTitle()).toEqual('Каталог Onliner.by');
     });
   });
+
+
+  // routine functions
+  
+  function pathToMobiles() {
+    var element = browser.element(by.linkText("Каталог"));
+         element.click();
+
+    var element = browser.element(by.linkText("Мобильные телефоны"));
+        browser.wait(EC.elementToBeClickable(element), 5000)
+          .then(()=> {
+            element.click();
+          })
+        expect(browser.getTitle()).toEqual('Мобильный телефон купить в Минске');
+  }
+
+  function settingUpFilter(tag) {
+    var cb = browser.element.all(by.cssContainingText('span', tag)).first();
+    browser.driver.executeScript("arguments[0].scrollIntoView();", cb.getWebElement())
+      .then(()=> {
+         cb.click();
+      })
+      .then(()=> {
+        var condition = EC.elementToBeClickable(browser.$('.schema-tags__text'));
+           browser.wait(condition, 5000);
+      })
+  }
+
+  function findPhone(model) {
+    // crutch
+    browser.sleep(2000);
+
+    var element = browser.element(by.cssContainingText('span', model));
+    browser.wait(EC.presenceOf(element), 8000).then(()=> {
+        browser.executeScript("arguments[0].scrollIntoView();", element);
+        return element.click();
+    })
+    expect(browser.getTitle()).toEqual('Смартфон ' + model + ' купить в Минске');
+  }
+
+  function addToComparison() {
+    var element = browser.$('#product-compare-control');
+    browser.wait(EC.presenceOf(element), 5000).then(()=> {
+      element.click();
+    });
+
+    var panel = browser.$('.compare-button__sub.compare-button__sub_main');
+      return panel.getText()
+        .then((text)=> {
+          console.log("panel text: ", text);
+          return text;
+      });
+  }
 
 });
